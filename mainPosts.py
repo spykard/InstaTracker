f92f3dd6c24769f7c8cdfc0346db1f33ada4f231
spykard <kardakis@ceid.upatras.gr>
2021-06-07 18:47:32 +0300

    v1.0 - Ready

--- mainPosts.py
+++ mainPosts.py
@@ -4,6 +4,7 @@ import configparser
 import json
 import urllib.request
 import csv
+from collections import defaultdict, OrderedDict
 
 def vpn_check():
     ''' Checks if a VPN is currently running based on IP '''
@@ -36,56 +37,96 @@ def posts_parse(InstaLoader):
 
     return (posts_list)
 
-def get_and_store_likes(post):
-    ''' Gets a list of all the Profiles that liked the given Post and stores them on a File '''
+def get_and_store_likes(post, data, data_columnnames, data_fastset, InstaLoader):
+    ''' Gets a list of all the Profiles that liked the given Post and stores them on a Dict data Structure '''
+
+    count = 0
 
     print(f"Processing {post}...")
     
     profiles_list = post.get_likes()
 
+    data = defaultdict(OrderedDict,data)  # A more appropriate data type for direct assignment of values
 
+    # Update Structure
+    for profile in profiles_list:
+        id = profile.userid
+        temp_profile = instaloader.Profile.from_username(InstaLoader.context, 'yrwini')
 
-    with open('data/temp.txt', "a+") as f:
-        for profile in profiles_list:
-            f.write(str(profile.userid) + '\n')
+        # Debug
+        # print(temp_profile.username)
+        # print(temp_profile.has_public_story)
+        # print(temp_profile.has_viewable_story)
+        # quit()
 
-    print(f"Updated output File.")
+        if id in data_fastset:  # Existing Row
+            data[id][data_columnnames[0]] = "https://www.instagram.com/" + profile.username + "/"
+            data[id][data_columnnames[1]] = profile.userid
+            data[id][data_columnnames[2]] = profile.username
+            data[id][data_columnnames[3]] = not profile.is_private
+            data[id][data_columnnames[4]] = data[id][data_columnnames[4]] + ", " + post.owner_username + post.shortcode
+            data[id][data_columnnames[5]] = data[id][data_columnnames[5]]
+            data[id][data_columnnames[6]] = profile.has_public_story
+            data[id][data_columnnames[7]] = data[id][data_columnnames[7]]
+            data[id][data_columnnames[8]] = data[id][data_columnnames[8]]
+            print()    
+        else:                   # New Row
+            data[id][data_columnnames[0]] = "https://www.instagram.com/" + profile.username + "/"
+            data[id][data_columnnames[1]] = profile.userid
+            data[id][data_columnnames[2]] = profile.username
+            data[id][data_columnnames[3]] = not profile.is_private
+            data[id][data_columnnames[4]] = post.owner_username + post.shortcode
+            data[id][data_columnnames[5]] = ""
+            data[id][data_columnnames[6]] = profile.has_public_story
+            data[id][data_columnnames[7]] = ""
+            data[id][data_columnnames[8]] = ""
+            print()   
+
+        print(data[id])
+        count += 1
+        if count == 2:
+            break
+
+    return (data)
+
+def load_csv():
+    ''' Load/Read Input .CSV '''
+
+    f = open("data/source.csv", "r", newline='')
+    csv_reader = csv.DictReader(f)
+    data = {}
+    data_columnnames = ['instagram_url','instagram_id','instagram_name','is_public','done_posts','high_priority','has_story_available','score','notes']
+
+    for row in csv_reader:
+        data[int(row['instagram_id'])] = row
+
+    data_fastset = set(data.keys())  # A 'set' object of just the IDs
+
+    if len(data_columnnames) == len(data[next(iter(data))]):
+        print(f"Number of columns of input .csv matches the expected value: {len(data_columnnames)}")
+    else:
+        raise Exception(f"Error, Number of columns of input .csv does not match the expected value. Change the 'data_columnnames' variable.")
+
+    f.close()
+    print(f"CSV file successfully loaded.")
+
+    return (data, data_columnnames, data_fastset)
+
+def save_csv(data, data_columnnames):
+    ''' Write to Output .CSV '''
+
+    f = open("data/source.csv", "w", newline='')
+    csv_writer = csv.DictWriter(f, fieldnames=data_columnnames)
+    csv_writer.writeheader()
+    for row in list(data.values()):
+        csv_writer.writerow(row)
+
+    f.close()
+    print(f"Successfully wrote to CSV file.")
 
 
 ''' Run Main Function '''
-# Load/Read Input .CSV 
-f = open("data/source.csv", "r", newline='')
-csv_reader = csv.DictReader(f)
-data = {}
-data_columnnames = ['instagram_url','instagram_id','instagram_name','is_public','done_posts','high_priority','has_story_available','score','notes']
-
-for row in csv_reader:
-    data[int(row['instagram_id'])] = row
-
-data_fastset = set(data.keys())  # A 'set' object of just the IDs
-
-if len(data_columnnames) == len(data[next(iter(data))]):
-    print(f"Number of columns of input .csv matches the expected value: {len(data_columnnames)}")
-else:
-    raise Exception(f"Error, Number of columns of input .csv does not match the expected value. Change the 'data_columnnames' variable.")
-
-print(f"CSV file successfully loaded.")
-
-# Write to Output .CSV 
-f = open("data/source.csv", "w", newline='')
-print(list(data.values()))
-csv_writer = csv.DictWriter(f, fieldnames=data_columnnames)
-csv_writer.writeheader()
-for row in list(data.values()):
-    csv_writer.writerow(row)
-f.close()
-
-
-quit()
-
-
 print(f"Starting...")
-
 vpn_check()
 
 # Config Parsing
@@ -103,18 +144,14 @@ else:
     InstaLoader.login(config['Settings']['username'], config['Settings']['password'])  # Traditional log in Method, bugged https://github.com/instaloader/instaloader/issues/1150
 print(f"Logged in successfully.\n")
 
-# Load/Read Input .CSV 
-...
+# Load/Read Input .CSV
+(data, data_columnnames, data_fastset) = load_csv()
 
+# Parse Source Posts
 posts_list = posts_parse(InstaLoader)
 
 for post in posts_list:
-    get_and_store_likes(post)
+    data = get_and_store_likes(post, data, data_columnnames, data_fastset, InstaLoader)
 
-
-
-
-# INSTAGRAM SCRAPER
-# instagram-scraper j.touni -u thodoris_xrysikos -p fagaabvadaa2xC3Casd2 -t story --filter-location 109790383963906 --include-location --location
-# instagram-scraper --tag athens -u thodoris_xrysikos -p fagaabvadaa2xC3Casd2 -t story --include-location --filter-location 109790383963906
-# instagram-scraper trolololer86 -u thodoris_xrysikos -p fagaabvadaa2xC3Casd2 -t story --filter-location 14294616 --include-location --location
+# Write to Output .CSV
+save_csv(data, data_columnnames)
